e2db78ab31097a43d4401750fb6697e619080e97
MamboBryan <mambobryan@gmail.com>
2019-05-17 19:59:27 +0300

    Added next button id and linked to open Main Activity

--- app/src/main/java/com/bennjoroge/collab_test/LandingActivity.java
+++ app/src/main/java/com/bennjoroge/collab_test/LandingActivity.java
@@ -1,8 +1,11 @@
 package com.bennjoroge.collab_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,6 +14,7 @@ public class LandingActivity extends AppCompatActivity {
     String mUsername;
     String mMonth;
     Integer mNumber;
+    private Button nextActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,23 @@ public class LandingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_landing);
 
         getEditTextsData();
+
+        nextActivityButton = findViewById(R.id.next_activity_button);
+        nextActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAnotherActivity();
+            }
+        });
+    }
+
+    private void openAnotherActivity() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        myIntent.putExtra("username", mUsername);
+        myIntent.putExtra("month", mMonth);
+        myIntent.putExtra("number", mNumber);
+        startActivity(myIntent);
+
     }
 
     private boolean getEditTextsData() {
@@ -30,7 +51,7 @@ public class LandingActivity extends AppCompatActivity {
         mMonth = monthEditText.getText().toString();
         mNumber = Integer.parseInt(numberEditText.getText().toString());
 
-        if (!checkIfStringIsEmpty(mUsername, mMonth, mNumber)){
+        if (!checkIfStringIsEmpty(mUsername, mMonth, mNumber)) {
             Toast.makeText(this, "Fill all the fields!", Toast.LENGTH_SHORT).show();
             return false;
         } else {
